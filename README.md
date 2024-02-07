e68a5079fce1eee6bb99957384d99c1712f8b67a
kjh7825 <kjh7825.dev@gmail.com>
2024-02-07 14:38:51 +0900

    Revert "first commit"
    This reverts commit ddf9fa927a55d85d6719cf6add9737738b7b8721.

--- README.md
+++ README.md
@@ -74,4 +74,3 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 # react_setting
 # react_setting
 # react_setting
-# MyFirstProject_Frontend
--- src/pages/Main/container/MainContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MainPresenter from "../view/MainPresenter";
-
-const MainContainer = () => {
-    return <MainPresenter></MainPresenter>;
-};
-
-export default MainContainer;
